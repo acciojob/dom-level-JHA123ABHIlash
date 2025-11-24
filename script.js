818aba66991b916788082eeb12157068dc323fcf
Abhilash Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2025-11-24 19:47:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 
-let level=document.qeurySelector('#level');
+let level=document.querySelector('#level');
 let cnt=1;
 while(level.tagName!=='HTML'){
 level=level.parentElement;
 	cnt++;
 }
-alert('The level of the element is:',cnt);
+alert('The level of the element is:'+cnt);
 
