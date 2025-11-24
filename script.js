b4d1476e61f37893f35da6e0cac081ac63fade0d
Abhilash Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2025-11-24 19:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let level=document.qeurySelector('#level');
+let cnt=1;
+while(level.tagName!=='HTML'){
+level=level.parentElement;
+	cnt++;
+}
+alert('The level of the element is:',cnt);
+
